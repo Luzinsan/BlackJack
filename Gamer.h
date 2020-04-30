18df06938d1072fbe3914ce20becebc2ea6100f9
Luzinsan <53607329+Luzinsan@users.noreply.github.com>
2020-04-30 09:14:04 +0600

    Create Gamer.h

---
+++ Gamer.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Deck.h"
+
+class Gamer
+{
+private:
+	int m_win{ 0 };
+	int m_push{ 0 };
+	int m_losing_games{ 0 };
+	int m_bank;
+	int m_age{ 0 };
+	int m_bet;
+	std::string m_name = "Jack";
+public:
+	Gamer();
+	void value_f(int* parameter);
+	bool question();
+	void menu();
+	void playBlackjack(Deck& deck);
+};
